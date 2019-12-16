cd44ff2255ea96a5a3d3f55e86aaee6e7ee07d23
dayne beronque <dayneyulber@gmail.com>
2019-12-16 10:57:13 -0600

    changes

--- my-app/src/Components/About.js
+++ my-app/src/Components/About.js
@@ -4,13 +4,9 @@ class About extends Component {
   render() {
 
     if(this.props.data){
-      var name = this.props.data.name;
+      var contactdescript = this.props.data.contactdescript;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
@@ -28,12 +24,9 @@ class About extends Component {
             <p>{bio}</p>
             <div className="row">
                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
+                  <h2>Contact</h2>
                   <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
+						   <span>{contactdescript}</span><br />
 						   <span>{phone}</span><br />
                      <span>{email}</span>
 					   </p>
